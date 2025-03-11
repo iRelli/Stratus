e0d3a6ea3160612f0d656b34494995e9ffa42ea7
iRelli <79234459+iRelli@users.noreply.github.com>
2025-03-11 06:02:27 +0000

    updated

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -5,34 +5,39 @@ const { ChannelType } = require('discord.js');
 module.exports = {
   name: 'voiceStateUpdate',
   async execute(oldState, newState) {
-    if(!oldState.channel && newState.channel){
-      const serverData = await VoiceChannelCreate.findOne({ guildId: newState.guild.id})
-      if(!serverData || newState.channel.id !== serverData.channel) return;
+    if (!oldState.channel && newState.channel) {
+      const serverData = await VoiceChannelCreate.findOne({ guildId: newState.guild.id });
+      if (!serverData || newState.channel.id !== serverData.channelId) return;
 
+      const tempChannelName = serverData.name.replace('{username}', newState.member.user.username);
       const channel = await newState.guild.channels.create({
-        name: serverData.name,
+        name: tempChannelName,
         type: ChannelType.GuildVoice,
         parent: serverData.categoryId,
-        userLimit: serverData.limit || 0
-      })
-
-      await VoiceChannelUser.create({
-          guildId: newState.guild.id,
-          userId: newState.member.id,
-          channelId: channel.id
+        userLimit: serverData.limit || 0,
       });
 
-      await newState.member.voice.setChannel(channel).catch(err =>{});
+      await VoiceChannelUser.create({
+        guildId: newState.guild.id,
+        userId: newState.member.id,
+        channelId: channel.id,
+      });
+
+      await newState.member.voice.setChannel(channel).catch(err => {
+        console.error('Error moving user to the new channel:', err);
+      });
     }
 
-    if(oldState.channel && !newState.channel){
-      const userData = await VoiceChannelUser.findOne({channelId: oldState.channel.id});
-      if(!userData) return;
+    if (oldState.channel && !newState.channel) {
+      const userData = await VoiceChannelUser.findOne({ channelId: oldState.channel.id });
+      if (!userData) return;
 
-      const channel = await oldState.guild.channel.resolve(oldState.channel.id)
-      if(channel && channel.member.size === 0){
-        await VoiceChannelUser.deleteOne({channelId: oldState.channel.id})
-        await channel.delete().catch(err => {});
+      const channel = await oldState.guild.channels.resolve(oldState.channel.id);
+      if (channel && channel.members.size === 0) {
+        await VoiceChannelUser.deleteOne({ channelId: oldState.channel.id });
+        await channel.delete().catch(err => {
+          console.error('Error deleting the empty channel:', err);
+        });
       }
     }
   },

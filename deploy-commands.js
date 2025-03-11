e0c900cc64af307d89c2f7aa1905bacaca039b5d
iRelli <79234459+iRelli@users.noreply.github.com>
2025-03-11 04:25:37 +0000

    test

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,25 +3,25 @@ const { REST, Routes } = require('discord.js');
 const { readdirSync } = require('fs');
 const path = require('path');
 
-const commands = [];
-const commandFolders = readdirSync(path.join(__dirname, 'commands'));
+const deployCommands = async () => {
+  const commands = [];
+  const commandFolders = readdirSync(path.join(__dirname, 'commands'));
 
-for (const folder of commandFolders) {
-  const commandFiles = readdirSync(
-    path.join(__dirname, `commands/${folder}`),
-  ).filter((file) => file.endsWith('.js'));
+  for (const folder of commandFolders) {
+    const commandFiles = readdirSync(
+      path.join(__dirname, `commands/${folder}`),
+    ).filter((file) => file.endsWith('.js'));
 
-  for (const file of commandFiles) {
-    const command = require(`./commands/${folder}/${file}`);
-    if (command.data) {
-      commands.push(command.data.toJSON());
+    for (const file of commandFiles) {
+      const command = require(`./commands/${folder}/${file}`);
+      if (command.data) {
+        commands.push(command.data.toJSON());
+      }
     }
   }
-}
 
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+  const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-(async () => {
   try {
     console.log(`🚀 Refreshing ${commands.length} application (/) commands...`);
     await rest.put(
@@ -29,8 +29,9 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
       { body: commands },
     );
     console.log('✅ Successfully reloaded application (/) commands.');
-    process.exit(0);
   } catch (error) {
     console.error('❌ Error loading commands:', error);
   }
-})();
+};
+
+module.exports = { deployCommands };
